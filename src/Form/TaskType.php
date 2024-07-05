fe98f8c95e50a46499713f44604a4d093ce86cd3
Eric <e.gigondan@gmail.com>
2024-07-05 16:12:31 +0200

    edit task type

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -3,31 +3,26 @@
 namespace App\Form;
 
 use App\Entity\Task;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class TaskType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('isDone')
-            ->add('doneAt', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
-            ])
-        ;
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+
+                ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu de votre tâche',
+                ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
