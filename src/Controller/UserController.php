17817ac6fcde322373814936862860bf0b75922e
Eric <e.gigondan@gmail.com>
2024-07-06 13:48:14 +0200

    [ADD] controller

---
+++ src/Controller/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TaskRepository;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/admin', name: 'app_admin')]
+    /**
+     * Route pour la liste des utilisateurs
+     * @return Response
+     *
+     */
+    #[Route('/', name: 'user')]
+    public function index(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+        return $this->render('user/index.html.twig', [
+            'users' => $users
+        ]);
+    }
+}
